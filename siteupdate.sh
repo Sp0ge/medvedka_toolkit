8df8581782c6d39bf2f637e19a79edae2a16aa0a
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2022-06-24 22:15:56 +0300

    fix

--- siteupdate.sh
+++ siteupdate.sh
@@ -3,6 +3,7 @@ echo remove medvedka_toolkit dir
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 sudo apt-get install python3 python3-pip
 sudo git clone https://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@github.com/Sp0ge/medvedka_toolkit.git
-sudo python3 -m pip install -r medvedka_toolkit/requirements.txt
+cd medvedka_toolkit
+sudo python3 -m pip install -r requirements.txt
 echo pip intalled
-sudo screen python3 medvedka_toolkit/main.py
+sudo screen python3 main.py
