61061d409fa9177c1a875f59eb1ba501726b3a9c
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2022-06-24 22:14:34 +0300

    fix

--- siteupdate.sh
+++ siteupdate.sh
@@ -5,4 +5,4 @@ sudo apt-get install python3 python3-pip
 sudo git clone https://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@github.com/Sp0ge/medvedka_toolkit.git
 sudo python3 -m pip install -r medvedka_toolkit/requirements.txt
 echo pip intalled
-sudo screen python3 main.py
+sudo screen python3 medvedka_toolkit/main.py
