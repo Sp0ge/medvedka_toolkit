4f391d39af2b14de98b444c24bc393ec5ee97e30
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2022-06-25 15:19:23 +0300

    social finder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
+from requests import request
+import requests
 import vk_api
 import random
 import urllib.request, json
@@ -9,10 +11,21 @@ uquestions = []
 uanswers = []
 utoken = ''
 lsat = ""
-
-
 def send(answer):
     vk.method("messages.send", {"peer_id": peer_id, "message": answer, "random_id": random.randint(1, 2147483647)})
+
+def proxies():
+    all_proxies = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt').text.splitlines()
+    i=0
+    check = False
+    while not chec:
+        i+=1
+        proxy = all_proxies[i]
+        chec = requests.get("https://vk.com",{'https': 'http://%s' % (proxy)})
+    return proxy
+
+
+
 
 
 def phoneget(phone):
@@ -38,8 +51,17 @@ def ipget(getIP):
     send("Вывод:")
     infoList = json.load(getInfo)
     send(f"IP: {infoList['ip']} \n City: {infoList['city']} \n Region: {infoList['region']} \n Country: {infoList['country']} \n Hostname: {infoList['hostname']} ")
-    
 
+def cs(username):
+    uls=[f'https://vk.com/{username}',f'https://www.youtube.com/user/{username}',f'https://www.pinterest.ru/{username}',f'https://www.twitch.tv/{username}',f'https://likee.video/@{username}',f'https://ok.ru/{username}',f'https://www.github.com/{username}',f'https://www.tiktok.com/@{username}']
+    print("Проверка...")
+    for site in uls:
+        try:
+            resp = requests.get(site)
+            if str(resp.status_code) == "200":
+                send(str(site))
+        except:
+            print('error')
 
 
 vk = vk_api.VkApi(token=TOKEN)
@@ -56,7 +78,8 @@ while True:
                 send('Функции:')
                 #send('Спам \n информация о номере \n Пример команды: \n спам +7********* ')
                 send("Информация о номере. \n Пример команды: \n +7********* ")
-                send("Информация о ip адресе. \n Пример команды: \n ip 127.0.0.1 ")
+                send("Информация о ip адресе. \n Пример команды: \n ip 0.0.0.0 ")
+                send("Поиск по соцсетям. \n Пример команды: \n social mishka324 ")
                 
                 
             elif "+" in text:
@@ -69,6 +92,13 @@ while True:
                 text = text.replace('ip ','')
                 print(text)
                 ipget(text)
+                
+            elif "social" in text:
+                text = text.replace('social ','')
+                username = text
+                cs(username)
+
+                
             else:
                 send("Неверная команда!")
     except Exception as E:
